e031004229e11ce70c5eaef26c4cd96edab2df31
monodara <642649909@qq.com>
2021-11-24 15:14:19 +0000

    Add some style to the Food and SearchList components.

--- src/search/SearchList.js
+++ src/search/SearchList.js
@@ -1,15 +1,21 @@
 import { Component } from "react";
 import Food from "./Food";
-import getFoodList from "../api";
+// import getFoodList from "../api";
+
 
 class SearchList extends Component {
+    
     render() {
+        
         return (
-        <div className="foodList">
-            <h1>Food</h1>
+            
+        <div className="foodList" >
+            <h3 style={{color:"blue",textAlign:"center"}}>Here are your search results</h3>
+            <hr />
             {this.props.foodList.map((f)=>(
                 <div key={f.id}>
-                    <Food name={f.name} calories={f.calories}/>
+                    <Food id={f.id} name={f.name} calories={f.cal} photo={f.photo} quantity={f.qty}/>
+                    
                 </div>
             ))}
             
